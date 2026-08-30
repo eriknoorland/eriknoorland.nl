939cc4142cfb9dbc8df9b6eb403d9ba98d06f1e8
Erik Noorland <noorlandjr_e@hotmail.com>
2026-08-30 17:47:37 +0200

    Performance improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -188,10 +188,12 @@ export const Head: HeadFC = () => <>
   <html lang="en" />
   <title>Erik Noorland - Frontend Developer</title>
   <meta name="description" content="The portfolio of a passionate frontend developer" />
+  <link rel="preload" as="font" type="font/woff2" href="/fonts/roboto-v20-latin/roboto-v20-latin-300.woff2" crossOrigin="anonymous" />
+  <link rel="preload" as="font" type="font/woff2" href="/fonts/roboto-v20-latin/roboto-v20-latin-regular.woff2" crossOrigin="anonymous" />
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-  <link rel="manifest" href="/site.webmanifest" />
+  <link rel="manifest" href="/site.webmanifest" fetchpriority="low" />
   <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
   <meta name="msapplication-TileColor" content="#ffffff" />
   <meta name="theme-color" content="#ffffff"></meta>
