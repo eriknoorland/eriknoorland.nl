f0bdda71195ef79a48719fb968be94c04eb2cdff
Erik Noorland <noorlandjr_e@hotmail.com>
2026-09-11 16:54:15 +0200

    Update React and Typescript. Also make sure the Node version is locked in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -193,7 +193,7 @@ export const Head: HeadFC = () => <>
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-  <link rel="manifest" href="/site.webmanifest" fetchpriority="low" />
+  <link rel="manifest" href="/site.webmanifest" fetchPriority="low" />
   <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
   <meta name="msapplication-TileColor" content="#ffffff" />
   <meta name="theme-color" content="#ffffff"></meta>
